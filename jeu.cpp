b63da8742ba593a88e3c8a77a8af74b7dcebf19a
apeinot <alexandre.peinot@gmail.com>
2018-03-19 08:43:53 +0100

    début code afichage

--- jeu.cpp
+++ jeu.cpp
@@ -4,3 +4,19 @@ Jeu::Jeu(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Jeu::afficherPlateau()
+{
+    for (int i=0; i<plateau.getTaille(), i++)
+    {
+        for (int j=0; j<plateau.getTaille(), j++)
+        {
+
+        }
+    }
+}
+
+QString Jeu::readCase(int i, int j)
+{
+    return QString::number(plateau.getCase(i,j));
+}
