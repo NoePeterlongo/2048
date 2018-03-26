dcce47e55cbed63a415299ab190739a0d3591a59
apeinot <alexandre.peinot@gmail.com>
2018-03-26 11:10:15 +0200

    ajout de couleur

--- jeu.h
+++ jeu.h
@@ -37,7 +37,7 @@ private:
     int score = 0;
     int scoreDebug = 0;
     Plateau plateau;
-    QList<QString> Lcolor={"#F4CA99", "#FCFBFA","#E2BB8E", "#ED9530", "#DE5116", "#B10406", "#BE0933", "#AC28FE", "#4F24FD", "#138CBC", "#43D4C6", "#7ABE90"};
+    QList<QString> Lcolor={"#F4CA99", "#FCFBFA","#E2BB8E", "#ED9530", "#DE5116", "#B10406", "#BE0933", "#AC28FE", "#4F24FD", "#138CBC", "#43D4C6", "#7ABE90", "#A4E976", "#C2F60A", "#F6FB4A", "#C1BEB6"};
 
     Coup* coups[100000];
     Plateau* plateauInitial;
